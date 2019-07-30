57ee1fd2fcb3622ffc7913609678a0183f60a025
籍孟飞 <726616682@qq.com>
2019-07-30 10:59:37 +0800

    增加git过滤配置文件

--- CodeDemo/DetailView.swift
+++ CodeDemo/DetailView.swift
@@ -7,36 +7,47 @@
 //
 
 import SwiftUI
-
+import MapKit
 struct DetailView <Data>: View where Data: Item{
     var item: Data
     var body: some View {
-        
-        VStack {
-            
-            ScrollView(.horizontal, showsIndicators: false) {
-                
-                HStack {
-                    ForEach(0..<3) { index in
-                        Image(self.item.imageName)
-                            .resizable()
-                            .renderingMode(.original)
-                            .aspectRatio(contentMode: .fill)
-                            .background(Color.black)
-                            .frame(width: SW, height: 250)
-                            .padding(.trailing,10)
-                    }
-                }
-            }
-            
-            Image(item.imageName)
-                .resizable()
-                .renderingMode(.original)
-                .aspectRatio(contentMode: .fit)
-                .background(Color.black)
-        
-        }
+        MapView()
+            .navigationBarTitle("地图")
     
+    }
+}
+
+struct MapView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.showsUserLocation = true
+        mapView.userLocation.title  = "我的位置"
+        mapView.delegate = context.coordinator
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        print("=updateUIView")
+    }
+    
+    // 返回UIKit中的 时间协调器 如 delegate target/action
+    func makeCoordinator() -> Coordinator {
+       return Coordinator()
+    }
+}
+
+class Coordinator: NSObject {
+    
+}
+extension Coordinator: MKMapViewDelegate {
+    
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        print("结束加载地图")
+    }
+    
+    func mapViewWillStartLoadingMap(_ mapView: MKMapView) {
+        print("mapViewWillStartLoadingMap===============")
     }
 }
 
